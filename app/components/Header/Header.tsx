9ea7980a498ad35806e0e223f9ce7e7856abc9ff
Maxime <maxime13.p@gmail.com>
2023-08-14 11:02:00 +0200

    💄 add some responsive on home's Header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
@@ -9,8 +8,8 @@ const Header: React.FC = () => {
         <h1>Welcome to My Portfolio</h1>
       </li>
       <li className={styles.headerContent}>
-        <Image width={250} height={250} src="/maxime-parmentier-1.png" alt="Maxime Parmentier Profile Photo" className={styles.headerPhoto} />
-        <div className={styles.headerText}>
+        <img src="/maxime-parmentier-1.png" alt="Maxime Parmentier Profile Photo" className={styles.headerPhoto} />
+        <div className={styles.headerTexts}>
           <h2>Maxime Parmentier</h2>
           <p>Soon to be graduated Software Engineer - Cloud Computing DevOps Engineer</p>
         </div>
