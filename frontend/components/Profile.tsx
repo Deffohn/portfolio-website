d7448f86208817607b24bf5fad6cc8c0a24d72e1
Maxime <maxime13.p@gmail.com>
2023-10-20 10:07:27 +0200

    💄 change profile image

--- frontend/components/Profile.tsx
+++ frontend/components/Profile.tsx
@@ -6,11 +6,11 @@ const Profile: React.FC = () => {
     <div className='flex flex-col items-center justify-center gap-2 ml-2 mr-2'>
       <div className='flex flex-col items-center justify-center gap-2'>
         <Image
-          className='rounded-full w-full h-auto sm:w-1/2 sm:h-auto'
-          src='/maxime-parmentier-1.png'
+          className='rounded-[64px]'
+          src='/maxime-parmentier-2.png'
           alt='Profile Picture'
-          width={200}
-          height={200}
+          width={240}
+          height={240}
         />
         <h1 className='text-4xl font-bold text-center'>Maxime Parmentier</h1>
         <h2 className='text-2xl font-bold '>Software Engineer</h2>
