d57310721c2c0ff339b8401ae194ce39c6853a89
Maxime <maxime13.p@gmail.com>
2023-10-24 18:12:52 +0200

    💄 responsive navbar button

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,7 +1,82 @@
 "use client";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+const responsiveWidthlimit: number = 600;
 
 const Navbar: React.FC = () => {
+
+  const [screenWidth, setScreenWidth] = useState(responsiveWidthlimit + 1);
+  const [isNavbarMenuOpen, setIsNavbarMenuOpen] = useState(false);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", () => {
+      setScreenWidth(window.innerWidth);
+    });
+  }, []);
+
+  if (screenWidth < responsiveWidthlimit) {
+    if (isNavbarMenuOpen) {
+      return (
+        <div className='sticky top-0 bg-slate-200 border-b-2 border-slate-500 shadow-2xl mb-8'>
+          <div
+            className="flex flex-row gap-2 items-center h-full"
+          >
+            <div className="m-2 mt-4 ml-4 hover:bg-slate-500 text-black hover:text-slate-100">
+              <a
+                key="menu"
+                onClick={() => setIsNavbarMenuOpen(false)}
+              >
+                <Image
+                  className="m-2"
+                  src={"/cross-1.png"}
+                  alt={"Menu"}
+                  width={40}
+                  height={40}
+                />
+              </a>
+            </div>
+          </div>
+          
+          <nav>
+            <div className='ml-4 flex flex-col items-center'>
+              {navList.map((navItem) => (
+                <div className='flex items-center'>
+                  <NavbarSectionItem key={navItem.id} navItem={navItem}/>
+                </div>
+              ))}
+            </div>
+          </nav>
+        </div>
+        
+      );
+    } else {
+      return (
+        <div className='sticky top-0'>
+          <div
+            className="flex flex-row gap-2 items-center h-full"
+          >
+            <div className="m-2 mt-4 ml-4 hover:bg-slate-500 text-black hover:text-slate-100">
+              <a
+                key="menu"
+                onClick={() => setIsNavbarMenuOpen(true)}
+              >
+                <Image
+                  className="m-2"
+                  src={"/bars-1.png"}
+                  alt={"Menu"}
+                  width={40}
+                  height={40}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
   return (
     <nav className='flex sticky top-0 bg-slate-200 border-b-2 border-slate-500 shadow-2xl mb-8'>
       <div className='ml-4 flex flex-row items-center'>
@@ -18,7 +93,8 @@ const NavbarSectionItem: React.FC<{navItem: NavListItem}> = ({navItem}) => {
   return (
     <a
       key={navItem.id}
-      className="flex flex-row gap-2 items-center h-full hover:bg-slate-500 text-black hover:text-slate-100" href={navItem.href}
+      className="flex flex-row gap-2 items-center h-full hover:bg-slate-500 text-black hover:text-slate-100"
+      href={navItem.href}
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
     >
