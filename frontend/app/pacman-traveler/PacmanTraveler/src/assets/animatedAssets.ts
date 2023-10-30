3391638b7e643466b117f46da9f5d75a10a4a2a1
Maxime <maxime13.p@gmail.com>
2023-10-30 10:58:22 +0100

    ✨ new definition for drawable object with new orientation feature

---
+++ frontend/app/pacman-traveler/PacmanTraveler/src/assets/animatedAssets.ts
@@ -0,0 +1,23 @@
+export type animationSettings = {
+  totalFrames: number,
+  rotationDirection: -1 | 1,
+  state: number,
+}
+
+export type orientationSettings = {
+  totalCycleAngle: number, // 360
+  periodAngle: number,
+  state: number,
+}
+
+export type orientedSpriteImageSettings = {
+  pxWidth: number,
+  pxHeight: number,
+  image: HTMLImageElement,
+
+  imageSizeX: number;
+  imageSizeY: number;
+
+  animationSettings: animationSettings,
+  orientationSettings: orientationSettings,
+}
--- frontend/app/pacman-traveler/PacmanTraveler/src/objects/CogScore.ts
+++ frontend/app/pacman-traveler/PacmanTraveler/src/objects/CogScore.ts
@@ -1,79 +1,23 @@
 import { CircleHitbox } from "../Hitboxs/CircleHitbox";
-import { Direction } from "../mapTypes";
+import { orientedSpriteImageSettings } from "../assets/animatedAssets";
+import { DrawableObject } from "./DrawableObject";
 import { Obstacle } from "./Obstacle";
 
-export class CogScore implements Obstacle {
-  hitbox: CircleHitbox;
-  animatedImage: {
-    image: HTMLImageElement,
-    totalFrames: number,
-    pxPeriod: number,
-    ySize: number,
-    rotationDirection: -1 | 1,
-    state: number,
-  };
-  imageSizeX: number;
-  imageSizeY: number;
-
+export class CogScore extends DrawableObject<CircleHitbox> implements Obstacle {
   score: number;
 
   constructor(
     x: number, y: number,
     hitboxRadius: number,
-    imageSizeX: number, imageSizeY: number,
-    imageForAnimation: string,
+    orientedSpriteImageSettings: orientedSpriteImageSettings,
     scoring: number,
-    animationTotalFrames: number,
-    animationPxPeriod: number,
-    animationYSize: number,
-    animationRotationDirection: -1 | 1,
-    stateToBegin: number,
   ) {
-    this.hitbox = new CircleHitbox(x, y, hitboxRadius);
-
-    this.animatedImage = {
-      image: new Image(),
-      totalFrames: animationTotalFrames,
-      pxPeriod: animationPxPeriod,
-      ySize: animationYSize,
-      rotationDirection: animationRotationDirection,
-      state: stateToBegin,
-    };
-    this.animatedImage.image.src = imageForAnimation;
-
-    this.imageSizeX = imageSizeX;
-    this.imageSizeY = imageSizeY;
+    super(
+      new CircleHitbox(x, y, hitboxRadius),
+      orientedSpriteImageSettings,
+    );
 
     this.score = scoring;
-  }
-  move(_direction: Direction, _speed: number, _obstacles: Obstacle[]): void {
-    return;
-  }
-
-  canvasDraw(
-    ctx: CanvasRenderingContext2D,
-    tileWidthPx: number, tileHeightPx: number,
-    deltaPacmanXPx: number, deltaPacmanYPx: number,
-  ) {
-    // refresh animation state
-    this.animatedImage.state = (this.animatedImage.state + this.animatedImage.rotationDirection) % this.animatedImage.totalFrames;
-    if (this.animatedImage.state < 0) this.animatedImage.state += this.animatedImage.totalFrames;
-
-    ctx.drawImage(
-      this.animatedImage.image,
-
-      // position in image
-      this.animatedImage.state * this.animatedImage.pxPeriod, 0,
-
-      // size in image
-      this.animatedImage.pxPeriod, this.animatedImage.ySize,
-
-      // position on canvas
-      (this.hitbox.x - this.imageSizeX) * tileWidthPx - deltaPacmanXPx, (this.hitbox.y - this.imageSizeY) * tileHeightPx - deltaPacmanYPx,
-
-      // size on canvas
-      tileWidthPx * 2 * this.imageSizeX, tileHeightPx * 2 * this.imageSizeY,
-    );
   }
 
 }
